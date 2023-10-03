11b7017e251744b6ddcbee466c6317aafb5f075d
Sahil <leviack629@gmail.com>
2023-10-03 12:11:55 +0530

    header and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+    <footer className='flex mx-auto items-center justify-center text-center bottom-0 fixed w-full bg-gray-200 px-4 py-3'>
+      <p>&copy; 2023 All Rights Reserved</p>
+    </footer>
+    </>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <header className='sticky z-50 top-0'>
+      <nav className='bg-gray-200 px-4 lg:px-6 py-3'>
+        <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl font-bold'>
+          <Link to='/'>LOGO</Link>
+          <ul className='flex gap-4 font-medium'>
+            <li>
+              <NavLink to='/' className={({isActive}) => `${isActive ? "text-orange-600" : "text-black"} hover:text-orange-600`}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to='/about' className={({isActive}) => `${isActive ? "text-orange-600" : "text-black"} hover:text-orange-600`}>About</NavLink>
+            </li>
+            <li>
+              <NavLink to='/posts' className={({isActive}) => `${isActive ? "text-orange-600" : "text-black"} hover:text-orange-600`}>Posts</NavLink>
+            </li>
+            <li>
+              <NavLink to='/github' className={({isActive}) => `${isActive ? "text-orange-600" : "text-black"} hover:text-orange-600`}>Github</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    </>
   )
 }
 

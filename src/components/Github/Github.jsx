e0b536d909e5ffdc6b77aa32d205852b26f7e4d6
Sahil <leviack629@gmail.com>
2023-10-03 13:26:10 +0530

    github page

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Github = () => {
+const Github = () => {<h2>GitHub Details</h2>
+  const [data, setData] = useState([])
+  useEffect(()=> {
+    fetch('https://api.github.com/users/sxhil296')
+    .then(response => response.json())
+    .then((data) => {
+      setData(data)
+    })
+  },[])
   return (
-    <div>Github</div>
+    <>
+    {data ? (
+      <>
+      <main className="container flex mx-auto max-w-7xl justify-center mt-10">
+      
+      <div className='flex-1'>
+        <img src={data.avatar_url} alt="github pfp" width={350} className='rounded-lg' />
+      </div>
+      <div className='flex-1'>
+        <h3>{data.login}</h3>
+        <p>{data.bio}</p>
+      <div className="flex gap-3 mt-16">
+        <div className='flex flex-col'>
+          <span>Public Repos</span>
+          <span>{data.public_repos}</span>
+        </div>
+        <div className='flex flex-col'>
+          <span>Followers</span>
+          <span>{data.followers}</span>
+        </div>
+        <div className='flex flex-col'>
+          <span>Following</span>
+          <span>{data.following}</span>
+        </div>
+      </div>
+      </div>
+    </main>
+      </>
+    ) : (
+      <p>Loading...</p>
+    )
+    }
+    </>
   )
 }
 
